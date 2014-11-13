fef7446126d647156c99776642a7e51a8ccd0031
Ivan Morgillo <imorgillo@gmail.com>
2014-11-13 15:20:36 +0100

    Add foreground param to getPicture
    Using foreground = true CameraActivity will be used instead of the
    system camera app.

--- www/Camera.js
+++ www/Camera.js
@@ -59,37 +59,17 @@ cameraExport.getPicture = function(successCallback, errorCallback, options) {
     var popoverOptions = getValue(options.popoverOptions, null);
     var cameraDirection = getValue(options.cameraDirection, Camera.Direction.BACK);
 
-    var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
-                mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection];
-
-    exec(successCallback, errorCallback, "Camera", "takePicture", args);
-    // XXX: commented out
-    //return new CameraPopoverHandle();
-};
-
-cameraExport.getNewPicture = function(successCallback, errorCallback, options) {
-    argscheck.checkArgs('fFO', 'Camera.getPicture', arguments);
-    options = options || {};
-    var getValue = argscheck.getValue;
-
-    var quality = getValue(options.quality, 50);
-    var destinationType = getValue(options.destinationType, Camera.DestinationType.FILE_URI);
-    var sourceType = getValue(options.sourceType, Camera.PictureSourceType.CAMERA);
-    var targetWidth = getValue(options.targetWidth, -1);
-    var targetHeight = getValue(options.targetHeight, -1);
-    var encodingType = getValue(options.encodingType, Camera.EncodingType.JPEG);
-    var mediaType = getValue(options.mediaType, Camera.MediaType.PICTURE);
-    var allowEdit = !!options.allowEdit;
-    var correctOrientation = !!options.correctOrientation;
-    var saveToPhotoAlbum = !!options.saveToPhotoAlbum;
-    var popoverOptions = getValue(options.popoverOptions, null);
-    var cameraDirection = getValue(options.cameraDirection, Camera.Direction.BACK);
+    var foreground = getValue(options.foreground, false);
 
     var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
                 mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection];
 
-    exec(successCallback, errorCallback, "NewCamera", "takePicture", args);
-    return new CameraPopoverHandle();
+    if (foreground) {
+        exec(successCallback, errorCallback, "ForegroundCamera", "takePicture", args);
+        return new CameraPopoverHandle();
+    } else {
+        exec(successCallback, errorCallback, "Camera", "takePicture", args);
+    }
 };
 
 cameraExport.cleanup = function(successCallback, errorCallback) {
